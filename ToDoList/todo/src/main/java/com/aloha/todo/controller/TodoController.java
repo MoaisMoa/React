7624a9fa801e0785fa503aef70e40f86e0724475
MoaisMoa <mom.moom.mu@gmail.com>
2026-02-24 11:30:52 +0900

    2026-02-24 : TodoList feat: TodoController에 @CrossOrigin 추가

--- ToDoList/todo/src/main/java/com/aloha/todo/controller/TodoController.java
+++ ToDoList/todo/src/main/java/com/aloha/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,14 @@ import com.github.pagehelper.PageInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin(
+    origins = {
+    "http://localhost:3000",
+    "http://localhost:5173"
+    },
+    allowCredentials = "true",
+    maxAge = 3600
+)
 @Slf4j
 @RestController
 @RequestMapping("/todos")
