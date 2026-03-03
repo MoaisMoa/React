70d1838383959c8eb3430cce576f3fefa90d18b5
MoaisMoa <mom.moom.mu@gmail.com>
2026-03-03 17:24:22 +0900

    2026-03-03 : Board (Front)

--- Board/board/SQL/files.sql
+++ Board/board/SQL/files.sql
@@ -1,3 +1,4 @@
+-- Active: 1767840762825@@127.0.0.1@3306@aloha
 DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `no` BIGINT NOT NULL AUTO_INCREMENT,             -- 파일 번호 (자동증가)
@@ -13,3 +14,26 @@ CREATE TABLE `files` (
   `seq` BIGINT NULL DEFAULT 0,                  -- 순서
   PRIMARY KEY (`no`)                            -- PK 설정
 ) COMMENT='파일';
+
+-- sample Data
+-- 게시글 목록 100번 글의 첨부파일
+-- 메인 파일
+INSERT INTO `files` ( id, p_id, file_name, origin_name, file_path, file_size, type )
+VALUES(UUID(),
+    (SELECT id FROM boards WHERE no = 100),
+    'TEST_100_썸네일.png',
+    '썸네일.png',
+    'C:/upload/TEST_100_썸네일.png',
+    200*1000,
+    'MAIN'
+    )
+;
+
+-- 첨부파일 4개
+INSERT INTO `files` ( id, p_id, file_name, origin_name, file_path, file_size, type )
+VALUES 
+  (UUID(), (SELECT id FROM boards WHERE no = 100), 'TEST_100_썸네일.png','파일1.png','C:/upload/TEST_100_파일1.png',200*1000,'SUB'),
+  (UUID(), (SELECT id FROM boards WHERE no = 100), 'TEST_100_파일2.png','파일2.png','C:/upload/TEST_100_파일2.png',200*1000,'SUB'),
+  (UUID(), (SELECT id FROM boards WHERE no = 100), 'TEST_100_파일3.png','파일3.png','C:/upload/TEST_100_파일3.png',200*1000,'SUB'),
+  (UUID(), (SELECT id FROM boards WHERE no = 100), 'TEST_100_파일4.png','파일4.png','C:/upload/TEST_100_파일4.png',200*1000,'SUB')
+;
--- Board/board/src/main/java/com/aloha/board/controller/BoardController.java
+++ Board/board/src/main/java/com/aloha/board/controller/BoardController.java
@@ -61,10 +61,17 @@ public class BoardController {
   }
   
   @GetMapping("/{id}")
-  public ResponseEntity<?> getOne(@PathVariable("id") String id) {
+  public ResponseEntity<?> getOne(@PathVariable("id") String id, Files file) {
       try {
+        // 게시글
         Boards board = boardService.selectById(id);
-        return new ResponseEntity<>(board, HttpStatus.OK);
+        file.setPId(id);
+        // 파일 목록
+        List<Files> fileList = fileService.listByParent(file);
+        Map<String, Object> response = new HashMap<>();
+        response.put("board", board);
+        response.put("fileList", fileList);
+        return new ResponseEntity<>(response, HttpStatus.OK);
       } catch (Exception e) {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
       }
@@ -132,6 +139,7 @@ public class BoardController {
       // type : "MAIN" ➡ 메인파일 1개
       if( type.equals("MAIN") ) {
         Files mainFile = fileService.selectByType(file);
+        log.info("MAI~~N",mainFile);
         return new ResponseEntity<>(mainFile, HttpStatus.OK);
       }
       // type : "SUB", ? ➡ 타입볍 파일 목록
