b90ef7088cc9f1bfdc61147c9d05bfb6514cec90
MoaisMoa <mom.moom.mu@gmail.com>
2026-03-03 11:23:18 +0900

    2026-03-03 : Board (Front) + axios JS 생성(boards,files)

---
+++ Board/board-app/src/apis/boards.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const api = axios.create({ baseURL: '/api' })
+
+export const boardsApi = {
+    list: (page = 1, size = 10) => api.get('/boards', {params: {page, size}}),
+    select: (id) => api.get(`/boards/${id}`),
+    insert: (data, headers) => api.post('/boards', data, {headers}),
+    update: (data, headers) => api.put('/boards', data, {headers}),
+    remove: (id) => api.delete(`/boards/${id}`),
+}
