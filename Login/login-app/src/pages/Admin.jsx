ff00c7bfc1063b7e552b4ea7012dbb589c007fc3
MoaisMoa <mom.moom.mu@gmail.com>
2026-03-06 16:11:01 +0900

    2026-03-06 : JWT + React + 관리자페이지 (레이아웃 적용)

--- Login/login-app/src/pages/Admin.jsx
+++ Login/login-app/src/pages/Admin.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Layout from '../components/common/Layout'
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <Layout>
+        <h1>Admin</h1>
+        <hr />
+        <h2>관리자 화면</h2>
+        <p>이 페이지는 ROLE_ADMIN 권한이 있는 사용자만 접근할 수 있습니다.</p>
+    </Layout>
   )
 }
 
