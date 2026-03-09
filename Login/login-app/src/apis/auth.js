4f897024e90263a8106f83e0471bfe0ca49b2291
MoaisMoa <mom.moom.mu@gmail.com>
2026-03-09 10:46:50 +0900

    2026-03-09 : JWT + React + 공통 API

---
+++ Login/login-app/src/apis/auth.js
@@ -0,0 +1,19 @@
+import api from "./api";
+
+// 회원가입
+export const join = (data) => api.post('/users', data)
+
+// 로그인
+export const login = (username, password) => {
+    return api.post('/login', {username, password})
+}
+
+// 회원 정보
+// 쿠키의 JWT를 authrization: Bearer... 헤더에 자동으로 붙여서 보냄
+export const info = () => api.get('/users/info')
+
+// 회원 정보 수정
+export const update = (data) => api.put('/users', data)
+
+// 회원 탈퇴
+export const remove = (username) => api.delete(`/users/${username}`)
