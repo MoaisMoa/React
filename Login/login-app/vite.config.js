1a2b9331c648109e76ae4a7ae1c9aaae48a9754e
MoaisMoa <mom.moom.mu@gmail.com>
2026-03-09 11:18:22 +0900

    2026-03-09 : JWT + React + Proxy 설정

--- Login/login-app/vite.config.js
+++ Login/login-app/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+        '/api' : {
+          target: 'http://localhost:8080', //(port) 서버 주소
+          changeOrigin: true,               // 요청 헤더의 Host 변경
+          secure: false,                    // https 지원 여부
+          rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
